ce438b08d151b55c7e06cd8d8f7975c830bbcffb
FernandoBP <fernando.bahena.199764@gmail.com>
2025-06-14 09:21:40 -0600

    firt commit

---
+++ basic.js
@@ -0,0 +1,9 @@
+console.log('hola mundo')
+console.info('Informacion')
+console.warn('advertencia')
+console.error('Error')
+
+console.log(globalThis)
+
+globalThis.console.log('Quitando la magia')
+
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log(sum(7+4))
